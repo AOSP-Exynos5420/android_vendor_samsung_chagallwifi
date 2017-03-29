9b934058f3b478a8eb5234f0d87ec4c428032c11
deadman96385 <seanhoyt963@gmail.com>
2017-03-28 20:56:11 -0500

    Cleanup
    Use stock gpsd no hexedits

--- chagallwifi-vendor-blobs.mk
+++ chagallwifi-vendor-blobs.mk
@@ -12,42 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#RIL
-#libGLES_trace.so needed as else protobuf error
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagallwifi/proprietary/sbin/cbd:root/sbin/cbd
-
-#Bluetooth firmware
+# Bluetooth firmware
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/vendor/firmware/bcm4350_V0301.0601.hcd:system/vendor/firmware/bcm4350_V0301.0601.hcd
 
-#WiFi - Firmware
+# WiFi Firmware
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/etc/wifi/nvram_mfg.txt_4354_a0:system/etc/wifi/nvram_mfg.txt_4354_a0 \
     vendor/samsung/chagallwifi/proprietary/etc/wifi/nvram_mfg.txt_4354_a1:system/etc/wifi/nvram_mfg.txt_4354_a1 \
     vendor/samsung/chagallwifi/proprietary/etc/wifi/nvram_net.txt_4354_a0:system/etc/wifi/nvram_net.txt_4354_a0 \
-    vendor/samsung/chagallwifi/proprietary/etc/wifi/nvram_net.txt_4354_a1:system/etc/wifi/nvram_net.txt_4354_a1 \
+    vendor/samsung/chagallwifi/proprietary/etc/wifi/nvram_net.txt_4354_a1:system/etc/wifi/nvram_net.txt_4354_a1
 
-#Sensors
-#Binary file DBT-T800XXU1BOJ1-20151023095433/system/lib/hw/sensors.universal5420.so matches
-#Binary file DBT-T800XXU1BOJ1-20151023095433/system/lib/libak09911c.so matches
+# Sensors
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/lib/hw/sensors.universal5420.so:system/lib/hw/sensors.universal5420.so
 
-#GPS
+# GPS
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/bin/gpsd:system/bin/gpsd \
     vendor/samsung/chagallwifi/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so
 
-#GPS - Config TODO: Move to device
-PRODUCT_COPY_FILES += \
-    vendor/samsung/chagallwifi/proprietary/etc/gps.xml:system/etc/gps.xml
-
-#SSWAP - Needed?
+# SSWAP
 PRODUCT_COPY_FILES += \
     vendor/samsung/chagallwifi/proprietary/sbin/sswap:root/sbin/sswap
-
-#MediaDRM
-PRODUCT_COPY_FILES_NN += \
-    vendor/samsung/chagallwifi/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
-
